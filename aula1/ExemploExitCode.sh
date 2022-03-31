05b55beb3077561f1d2a37b91dfcbec812a3ea89
Mateus Silva <mateushenriquesjc203@hotmail.com>
2022-03-31 17:13:09 -0300

    update

---
+++ aula1/ExemploExitCode.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+ls /tmp/teste
+
+echo "Script Executado"
+
+ls /tmp/arquivo11.txt
+
+RETURN_CODE=$?
+
+echo "O codigo de retorno do ls foi $RETURN_CODE"
+
+
+
+
--- aula1/primeiroScript.sh
+++ aula1/primeiroScript.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
+######################################################
+#
+# primeiroScript.sh - Script de Exemplo do Curso
+#
+# Autor: Mateus Silva (mateushls01@gmail.com)
+# Data Criação: 27/03/2022
+#
+# Descrição: Script de exemplo que le a data e hora
+#
+# Exemplo de uso: ./primeiroScript.sh
+#
+# Alterações
+#	Dia 31/03/2022 - Inclusão dos comentarios
+#
+######################################################
+
+DATAHORA=$(date +%H:%M)
+ARQALUNOS="/home/mateus/Documentos/estudos/linux/shell_script/aula0/alunos.txt"
+
+
+# Função de leitura da data e hora
 clear
 echo "================= Meu Primeiro Script ====================="
 echo ""
-echo -n "Exibir data e hora atual: "
-date
+echo  "Exibir data e hora atual:  $DATAHORA"
+ 
 echo "==========================================================="
+echo "Listagem dos Alunos: " 
+sort $ARQALUNOS
+
+
+
 
